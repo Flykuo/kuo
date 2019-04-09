2c8de9729a05a61347a35b23b86004c2b0ad8846
25136 <25136@Flykuo>
2019-04-09 19:29:49 +0800

    19.26修改

--- gy/src/gy/m.java
+++ gy/src/gy/m.java
@@ -1,10 +1,15 @@
 package gy;
 
+import java.util.Scanner;
+
 public class m {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner (System.in);
+		while(sc.hasNext()) {
+			
+		}
 	}
 
 }
